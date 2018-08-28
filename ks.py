a4822a3742119dc8f3cea5c9c28c2d498e2af357
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-28 15:56:28 -0500

    Result Array to store values of index,capacity pair we've found to prevent from repeating the same computation.

--- ks.py
+++ ks.py
@@ -9,8 +9,16 @@ v = [5, 7, 4, 3, 5, 2, 30]
 # Weight Capacity of Knapsack
 c = 10
 
+# Result Array to store values of index,capacity pair we've found to prevent from repeating the same computation.
+results = []
+
+for i in range(0, len(w)):
+	results.append([])
+
+print(results)
 
 # Function knap_sack:
 	# Arguments: --indx--Index of current item, --capacity--Weight capacity of knapsack
 	# Value: Returns value of best permution of items in knapsack.
 def knap_sack(indx, capcity):
+
