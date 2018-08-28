128eb5ce904492c67da3a25acee90d70e90c883d
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-28 15:46:49 -0500

    Weight Capacity of Knapsack

--- ks.py
+++ ks.py
@@ -2,11 +2,15 @@ print("KnapSack!")
 
 # Weight of Items
 w = [3, 9, 2, 1, 7, 3, 11]
+
 # Value of Items
 v = [5, 7, 4, 3, 5, 2, 30]
+
+# Weight Capacity of Knapsack
+c = 10
+
 
 # Function knap_sack:
 	# Arguments: --indx--Index of current item, --capacity--Weight capacity of knapsack
 	# Value: Returns value of best permution of items in knapsack.
-
 def knap_sack(indx, capcity):
